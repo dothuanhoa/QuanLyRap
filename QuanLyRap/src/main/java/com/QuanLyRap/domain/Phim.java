534bfb608dd45ef442c1bdf9e7909285f2a24060
Thuan Hoa <dothuanhoa1811@gmail.com>
2025-05-10 17:24:54 +0700

    format

--- QuanLyRap/src/main/java/com/QuanLyRap/domain/Phim.java
+++ QuanLyRap/src/main/java/com/QuanLyRap/domain/Phim.java
@@ -2,7 +2,6 @@ package com.QuanLyRap.domain;
 
 import java.time.LocalDateTime;
 
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,60 +20,79 @@ public class Phim {
     private float giaMua;
     private int idTheLoai;
     private int idNcc;
+
     public int getIdPhim() {
         return idPhim;
     }
+
     public void setIdPhim(int idPhim) {
         this.idPhim = idPhim;
     }
+
     public String getTenPhim() {
         return tenPhim;
     }
+
     public void setTenPhim(String tenPhim) {
         this.tenPhim = tenPhim;
     }
+
     public String getNoiDung() {
         return noiDung;
     }
+
     public void setNoiDung(String noiDung) {
         this.noiDung = noiDung;
     }
+
     public int getThoiLuong() {
         return thoiLuong;
     }
+
     public void setThoiLuong(int thoiLuong) {
         this.thoiLuong = thoiLuong;
     }
+
     public String getDanhGia() {
         return danhGia;
     }
+
     public void setDanhGia(String danhGia) {
         this.danhGia = danhGia;
     }
+
     public LocalDateTime getNgayMua() {
         return ngayMua;
     }
+
     public void setNgayMua(LocalDateTime ngayMua) {
         this.ngayMua = ngayMua;
     }
+
     public float getGiaMua() {
         return giaMua;
     }
+
     public void setGiaMua(float giaMua) {
         this.giaMua = giaMua;
     }
+
     public int getIdTheLoai() {
         return idTheLoai;
     }
+
     public void setIdTheLoai(int idTheLoai) {
         this.idTheLoai = idTheLoai;
     }
+
     public int getIdNcc() {
         return idNcc;
     }
+
     public void setIdNcc(int idNcc) {
         this.idNcc = idNcc;
     }
+
     @Override
     public String toString() {
         return "Phim [idPhim=" + idPhim + ", tenPhim=" + tenPhim + ", noiDung=" + noiDung + ", thoiLuong=" + thoiLuong
@@ -82,9 +100,4 @@ public class Phim {
                 + ", idNcc=" + idNcc + "]";
     }
 
-    
-
-    
-    
-    
 }
