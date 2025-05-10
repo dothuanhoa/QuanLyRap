092c8aedca4b8181859735edd3130a98feee98af
Thuan Hoa <dothuanhoa1811@gmail.com>
2025-05-10 13:38:13 +0700

    push ghe, chuc vu, phan hoi

--- QuanLyRap/src/main/java/com/QuanLyRap/domain/ChucVu.java
+++ QuanLyRap/src/main/java/com/QuanLyRap/domain/ChucVu.java
@@ -1,5 +1,55 @@
 package com.QuanLyRap.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class ChucVu {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idChucVu;
+    private String tenCongViec;
+    private float luong;
+    private String moTa;
+
+    public int getIdChucVu() {
+        return idChucVu;
+    }
+
+    public void setIdChucVu(int idChucVu) {
+        this.idChucVu = idChucVu;
+    }
+
+    public String getTenCongViec() {
+        return tenCongViec;
+    }
+
+    public void setTenCongViec(String tenCongViec) {
+        this.tenCongViec = tenCongViec;
+    }
+
+    public float getLuong() {
+        return luong;
+    }
+
+    public void setLuong(float luong) {
+        this.luong = luong;
+    }
+
+    public String getMoTa() {
+        return moTa;
+    }
+
+    public void setMoTa(String moTa) {
+        this.moTa = moTa;
+    }
+
+    @Override
+    public String toString() {
+        return "ChucVu [idChucVu=" + idChucVu + ", tenCongViec=" + tenCongViec + ", luong=" + luong + ", moTa=" + moTa
+                + "]";
+    }
 
 }
--- QuanLyRap/src/main/java/com/QuanLyRap/domain/Ghe.java
+++ QuanLyRap/src/main/java/com/QuanLyRap/domain/Ghe.java
@@ -1,5 +1,64 @@
 package com.QuanLyRap.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Ghe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idGhe;
+    private String tenGhe;
+    private boolean tinhTrang;
+    private String idPhong;
+    private String idLoaiGhe;
+
+    public int getIdGhe() {
+        return idGhe;
+    }
+
+    public void setIdGhe(int idGhe) {
+        this.idGhe = idGhe;
+    }
+
+    public String getTenGhe() {
+        return tenGhe;
+    }
+
+    public void setTenGhe(String tenGhe) {
+        this.tenGhe = tenGhe;
+    }
+
+    public boolean isTinhTrang() {
+        return tinhTrang;
+    }
+
+    public void setTinhTrang(boolean tinhTrang) {
+        this.tinhTrang = tinhTrang;
+    }
+
+    public String getIdPhong() {
+        return idPhong;
+    }
+
+    public void setIdPhong(String idPhong) {
+        this.idPhong = idPhong;
+    }
+
+    public String getIdLoaiGhe() {
+        return idLoaiGhe;
+    }
+
+    public void setIdLoaiGhe(String idLoaiGhe) {
+        this.idLoaiGhe = idLoaiGhe;
+    }
+
+    @Override
+    public String toString() {
+        return "Ghe [idGhe=" + idGhe + ", tenGhe=" + tenGhe + ", tinhTrang=" + tinhTrang + ", idPhong=" + idPhong
+                + ", idLoaiGhe=" + idLoaiGhe + "]";
+    }
 
 }
